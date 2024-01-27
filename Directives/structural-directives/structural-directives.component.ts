1c809c765e094756c19ae700629ce9c4a137c95d
Rohit Deshmukh <112280707+drohit02@users.noreply.github.com>
2024-01-27 16:59:11 +0530

    Add files via upload

---
+++ Directives/structural-directives/structural-directives.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-structural-directives',
+  templateUrl: './structural-directives.component.html',
+  styleUrls: ['./structural-directives.component.css']
+})
+export class StructuralDirectivesComponent {
+
+   val :boolean = true;
+
+   colors:string[] = ["Orange","Black","White","Blue"];
+
+}
